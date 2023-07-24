12791e62b2a974596c47f21c88581bb7e48b712f
Yassa Taiseer <yassataiseer@gmail.com>
2023-07-23 22:08:41 -0400

    commit

--- parser/parser.go
+++ parser/parser.go
@@ -17,14 +17,67 @@ type Parser struct{
 func New(l *lexer.Lexer) *Parser{ //new parser, takes lexer as args
 	p := &Parser{l: l}
 
-	p.l.NextToken()
-	p.l.NextToken()
+	p.nextToken()
+	p.nextToken()
 
 	return p
 }
 
 
-func (p *Parser) ParseProgram() *ast.Program{
-	return nil
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+			}
+		p.nextToken()
+	}
+	return program
 }
 
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+		case token.LET:
+			return p.parseLetStatement()
+	default:
+		return nil
+	}	
+}
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+	if !p.expectPeek(token.IDENT) {
+	return nil
+	}
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+	if !p.expectPeek(token.ASSIGN) {
+	return nil
+	}
+	// TODO: We're skipping the expressions until we
+	// encounter a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+	p.nextToken()
+	}
+	return stmt
+	}
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+	}
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+	}
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		return false
+	}
+}
